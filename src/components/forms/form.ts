2c0647e8b6668abc0e710d56b05718b81dc3d85f
Оксана <oksana@cobalt42.ru>
2024-02-01 18:04:23 +0500

    Добавляет валидацию полей форм

--- src/components/forms/form.ts
+++ src/components/forms/form.ts
@@ -2,35 +2,62 @@ class AstroForm extends HTMLElement {
   constructor() {
     super();
 
-    const form: HTMLElement | null = this.querySelector(".form");
+    const form: HTMLFormElement | null = this.querySelector(".form");
     const btn: HTMLElement | null = this.querySelector(".form-submit");
 
     const requiredInputs: NodeListOf<HTMLInputElement> = form.querySelectorAll("input[required]");
 
     const checkInput = (input: HTMLInputElement): void => {
-      if (input.value) {
+      if (input.value && input.value.length) {
         input.classList.remove("input--invalid");
       } else {
         input.classList.add("input--invalid");
       }
     };
-    
-    const checkForm = (): void => {
-      requiredInputs.forEach((input: HTMLInputElement) => {
-        checkInput(input);
-      });
-    };
-    
+
     requiredInputs.forEach((input: HTMLInputElement) => {
       input.addEventListener("input", () => {
         checkInput(input);
       });
     });
     
+    const checkForm = (): number => {
+      requiredInputs.forEach((input: HTMLInputElement) => {
+        checkInput(input);
+      });
+
+      const errorInputs: NodeListOf<HTMLInputElement> = form.querySelectorAll(".input--invalid");
+      
+      return errorInputs.length;
+    };
+
+    function sendData() {
+      const XHR = new XMLHttpRequest();
+      const data = new FormData(form);
+    
+      // form.classList.add('callback__form--preloader');
+      // XHR.open('POST', url);
+      // XHR.send(data);
+      // XHR.onload = function () {
+      //   if (XHR.status != 200) {
+      //     console.log(`Ошибка ${XHR.status}: ${XHR.statusText}`);
+      //     form.classList.add('callback__form--error');
+      //   } else {
+      //     console.log(`Готово, получили ${XHR.response}`);
+      //     form.classList.add('callback__form--succes');
+      //   }
+      //   form.classList.remove('callback__form--preloader');
+      // };
+    }
+    
     btn.addEventListener("click", function (event: Event) {
       event.preventDefault();
     
-      checkForm();
+      const errorCount = checkForm();
+
+      if (!errorCount) {
+        sendData();
+      }
     });
   }
 }
