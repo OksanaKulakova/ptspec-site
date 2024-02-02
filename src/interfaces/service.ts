f9a1062f516fccde3b3b0cf69c124a2d65285d54
Оксана <oksana@cobalt42.ru>
2024-02-02 16:53:06 +0500

    Добавляет разметку для страницы услуги

--- src/interfaces/service.ts
+++ src/interfaces/service.ts
@@ -3,6 +3,10 @@ export default interface Service {
   attributes: {
     title: string;
     slug: string;
+    description: string;
+    textTitle: string;
+    text: string;
+    result: string;
     createdAt: string;
     updatedAt: string;
     publishedAt: string;
