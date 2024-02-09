486cb73b23fd8fa8282f9d1ef1f8a1149b184b43
Оксана <oksana@cobalt42.ru>
2024-02-09 13:48:10 +0500

    Добавялет англ страницу проекты

--- src/data/getProjects.ts
+++ src/data/getProjects.ts
@@ -1,8 +1,11 @@
 import fetchApi from "@lib/strapi";
 import type Project from "@interfaces/project";
 import replaceQuotes from "@utils/replaceQuotes";
+import { defaultLang } from "@i18n/ui";
+import { useTranslatedPath } from "@i18n/utils";
+import type { lang } from "@i18n/ui";
 
-export default async function getProjects() {
+export default async function getProjects(locale: lang = defaultLang) {
   const projects = await fetchApi<Project[]>({
     endpoint: "projects",
     wrappedByKey: "data",
@@ -12,13 +15,17 @@ export default async function getProjects() {
       "pagination[start]": "0",
       "pagination[limit]": "100",
       "sort[0]": "id",
+      "locale": locale,
     },
   });
+
+  const translatePath = useTranslatedPath(locale);
+  const path = translatePath("/projects/");
 
   const list = projects.map((item) => ({
     id: item.id,
     title: replaceQuotes(item.attributes.title),
-    href: `/projects/${item.attributes.slug}/`,
+    href: `${path}${item.attributes.slug}/`,
     slug: item.attributes.slug,
   }));
 
