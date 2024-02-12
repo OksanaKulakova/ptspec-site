b741659685b7fba6b3826f1c4b83a3d6c6e4fb68
Оксана <oksana@cobalt42.ru>
2024-02-12 11:07:23 +0500

    Добавляет перевод вакансий

--- src/data/getVacancies.ts
+++ src/data/getVacancies.ts
@@ -1,8 +1,11 @@
 import fetchApi from "@lib/strapi";
 import type Vacancy from "@interfaces/vacancy";
 import replaceQuotes from "@utils/replaceQuotes";
+import { defaultLang } from "@i18n/ui";
+import { useTranslatedPath } from "@i18n/utils";
+import type { lang } from "@i18n/ui";
 
-export default async function getvacancys() {
+export default async function getvacancys(locale: lang = defaultLang) {
   const vacancies = await fetchApi<Vacancy[]>({
     endpoint: "vacancies",
     wrappedByKey: "data",
@@ -10,13 +13,17 @@ export default async function getvacancys() {
       "fields[0]": "title",
       "fields[1]": "slug",
       "sort[0]": "id",
+      "locale": locale,
     },
   });
+
+  const translatePath = useTranslatedPath(locale);
+  const path = translatePath("/vacancies/");
 
   const list = vacancies.map((item) => ({
     id: item.id,
     title: replaceQuotes(item.attributes.title),
-    href: `/vacancies/${item.attributes.slug}/`,
+    href: `${path}${item.attributes.slug}/`,
     slug: item.attributes.slug,
   }));
 
